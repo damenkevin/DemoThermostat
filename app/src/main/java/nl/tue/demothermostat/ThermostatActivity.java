be18536b8681f21a30d677a047c07e9ba8528e83
gsmet <panatha13>
2017-06-16 15:02:21 +0200

    Things

--- app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
+++ app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
@@ -4,6 +4,10 @@ package nl.tue.demothermostat;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,6 +20,7 @@ public class ThermostatActivity extends Activity {
     int atemp = vtemp+50;
     TextView temp;
     SeekBar seekBar;
+    BottomNavigationView bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,6 +96,17 @@ public class ThermostatActivity extends Activity {
                     seekBar.setProgress(vtemp);
                 }
 
+            }
+        });
+
+        bottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.
+                }
+
+                return true;
             }
         });
 
