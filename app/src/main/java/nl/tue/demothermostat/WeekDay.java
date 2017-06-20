e590574009f8ef0f8fce415f6b5be18b1da03e57
damenkevin <kevindamen_1@hotmail.com>
2017-06-20 20:22:56 +0200

    weekday layout added

--- app/src/main/java/nl/tue/demothermostat/WeekDay.java
+++ app/src/main/java/nl/tue/demothermostat/WeekDay.java
@@ -15,7 +15,7 @@ import android.app.Activity;
 
 public class WeekDay extends AppCompatActivity implements
         View.OnClickListener{
-    TextView dayswitch1, dayswitch2, dayswitch3, dayswitch4, dayswitch5;
+    TextView dayswitch1, dayswitch2, dayswitch3, dayswitch4, dayswitch5, nightswitch1, nightswitch2, nightswitch3, nightswitch4, nightswitch5;
     TextView x;
     private int mDay, mHour, mMinute;
 
@@ -31,8 +31,18 @@ public class WeekDay extends AppCompatActivity implements
         dayswitch3.setOnClickListener(this);
         dayswitch4=(TextView) findViewById(R.id.dayswitch4);
         dayswitch4.setOnClickListener(this);
-        dayswitch5=(TextView) findViewById(R.id.dayswitch5);
+        dayswitch5=(TextView) findViewById(R.id.nightswitch5);
         dayswitch5.setOnClickListener(this);
+        nightswitch1=(TextView) findViewById(R.id.nightswitch1);
+        nightswitch1.setOnClickListener(this);
+        nightswitch2=(TextView) findViewById(R.id.nightswitch2);
+        nightswitch2.setOnClickListener(this);
+        nightswitch3=(TextView) findViewById(R.id.nightswitch3);
+        nightswitch3.setOnClickListener(this);
+        nightswitch4=(TextView) findViewById(R.id.nightswitch4);
+        nightswitch4.setOnClickListener(this);
+        nightswitch5=(TextView) findViewById(R.id.nightswitch5);
+        nightswitch5.setOnClickListener(this);
     }
 
     @Override
