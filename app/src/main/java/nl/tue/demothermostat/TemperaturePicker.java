5a010c17d47aa1f4508816cbfd8b4e205ad984f7
damenkevin <kevindamen_1@hotmail.com>
2017-06-25 11:13:25 +0200

    save button changed, temperaturepicker white space behind status bar fixed

--- app/src/main/java/nl/tue/demothermostat/TemperaturePicker.java
+++ app/src/main/java/nl/tue/demothermostat/TemperaturePicker.java
@@ -27,6 +27,9 @@ public class TemperaturePicker extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature_picker);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         ImageView bPlus = (ImageView) findViewById(R.id.bPlus);
         bPlus.setImageResource(R.drawable.add_button);
